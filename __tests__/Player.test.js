b421ad08cac10384be2a409b044a360fdc0d4e07
Samantha Wolff <samanthawolff07@gmail.com>
2022-04-03 14:41:39 -0500

    test and build players health methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,8 +2,6 @@ const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
 
 const Player = require('../lib/Player');
-// const { expect } = require('@jest/globals');
-// const { test } = require('picomatch');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -34,4 +32,33 @@ test('gets inventory from player or returns false', () => {
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
+});
+
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alove or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
 });
